84c785bf2f42752ad8093bec2ed6a009fc354685
Orest Mazghal <109895411+Lordfedot@users.noreply.github.com>
2023-08-23 18:18:49 +0300

    fix

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -53,7 +53,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
           </div>
         ))}
       </div>
-      <CldUploadWidget onUpload={onUpload} uploadPreset="t4drjppf">
+      <CldUploadWidget onUpload={onUpload} uploadPreset="cfm4zsri">
         {({ open }) => {
           const onClick = () => {
             open();
